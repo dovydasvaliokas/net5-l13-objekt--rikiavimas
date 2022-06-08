7b2affde40704c11264b61e9b90a85da6bd7677f
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-08 10:54:16 +0300

    Sukurta klase bei padaryta tos klases isvedimo funkcija ir isbandyta

---
+++ VeiksmaiSuObjektais/MinMaxVidurkioIeskojimas/Preke.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxVidurkioIeskojimas
+{
+    internal class Preke
+    {
+        // Kintamieji (PRIVATŪS BŪTINAI)
+        private int id;
+        private string pavadinimas;
+        private string aprasymas;
+        private decimal kaina;              // kainai geriau decimal nei double, nes jis yra tiksliau su mažesnio tikslumo skaičiais
+        private int kiekis;
+        private string gamintojas;
+
+        // Getteriai setteriai
+        public int Id { get => id; set => id = value; }
+        public string Pavadinimas { get => pavadinimas; set => pavadinimas = value; }
+        public string Aprasymas { get => aprasymas; set => aprasymas = value; }
+        public decimal Kaina { get => kaina; set => kaina = value; }
+        public int Kiekis { get => kiekis; set => kiekis = value; }
+        public string Gamintojas { get => gamintojas; set => gamintojas = value; }
+
+        // Konstruktoriai
+        // Pilnas konstruktorius
+        public Preke(int id, string pavadinimas, string aprasymas, decimal kaina, int kiekis, string gamintojas)
+        {
+            this.id = id;
+            this.pavadinimas = pavadinimas;
+            this.aprasymas = aprasymas;
+            this.kaina = kaina;
+            this.kiekis = kiekis;
+            this.gamintojas = gamintojas;
+        }
+
+        // Tuščias konstruktorius
+        public Preke()
+        {
+        }
+
+        //---------------------------- Klasės funkcijos/metodai -----------------------------------
+
+        // Overridintas ToString() metodas.
+        public override string? ToString()
+        {
+            string tekstas = "id: " + id + "; pavadinimas: " + pavadinimas + "; aprasymas: " + aprasymas + "; kaina: " + kaina + "; kiekis: " + kiekis + "; gamintojas: " + gamintojas;
+            
+            return tekstas;
+        }
+    }
+}
--- VeiksmaiSuObjektais/MinMaxVidurkioIeskojimas/Program.cs
+++ VeiksmaiSuObjektais/MinMaxVidurkioIeskojimas/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MinMaxVidurkioIeskojimas // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Preke preke = new Preke(10251, "Kėdė", "Medinė sulankstoma kėdė", 23.99m , 4, "UAB Baldai");
+
+            Console.WriteLine(preke.ToString());
+
+            
+        }
+
+
+    }
+}
